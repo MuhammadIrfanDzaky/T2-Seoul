54da8d626ae47e25597debdc8b2e00b814e77faa
mhdirfndzky <irfndzky@gmail.com>
2026-04-25 16:18:46 +0700

    feat: add new video "Typescript" to recordings list

--- js/script.js
+++ js/script.js
@@ -13,6 +13,7 @@ const recordings = [
     { date: "31 Mar 2026", title: "Assymetrical Grid",                                      link: "https://youtu.be/eETFgE8DQ20", videoId: "eETFgE8DQ20", category: "other" },
     { date: "11 Apr 2026", title: "Beginner Javascript",                                    link: "https://youtu.be/oo09sYmr2Y0", videoId: "oo09sYmr2Y0", category: "tl" },
     { date: "18 Apr 2026", title: "Advanced Javascript",                                    link: "https://youtu.be/jfHzwg6pIZw", videoId: "jfHzwg6pIZw", category: "tl" },
+    { date: "25 Apr 2026", title: "Typescript",                                             link: "https://youtu.be/qQ0XXDeGfW8", videoId: "qQ0XXDeGfW8", category: "tl" },
 ];
 
 // Category display order
